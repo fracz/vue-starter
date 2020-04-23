fd3341b0242c0554f7c66727c9d857ca91d91a5d
Thomas LINTANF <thomas.lintanf@efrei.net>
2020-04-23 10:46:54 +0200

    Created List of friend

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,46 @@
 <template>
   <div>
-    <h1>Clicked {{ count }} times.</h1>
-    <button @click="iAmClicked()">Click me!</button>
+    <div v-if="participantsList!=[]">
+      <h1>Participants list</h1>
+      <ol>
+        <li
+          v-for="(participant, index) in participantsList"
+          :key="index"
+        >{{ participant.firstname}} {{ participant.lastname }}</li>
+      </ol>
+    </div>
+
+    <h3>New participant</h3>
+    <form @submit.prevent="addNewParticipant()">
+      <label>Firstname</label>
+      <input v-model="firstname" type="text" />
+      <label>Lastname</label>
+      <input v-model="lastname" type="text" />
+      <button>Add new participant</button>
+    </form>
   </div>
 </template>
- 
+
 <script>
 export default {
   data() {
-    return { count: 0 };
+    return {
+      firstname: "",
+      lastname: "",
+      participantsList: []
+    };
   },
   methods: {
-    iAmClicked() {
-      this.count++;
+    addNewParticipant() {
+      console.log({
+        firstname: this.firstname,
+        lastname: this.lastname
+      });
+
+      this.participantsList.push({
+        firstname: this.firstname,
+        lastname: this.lastname
+      });
     }
   }
 };
