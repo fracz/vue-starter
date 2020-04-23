bd4db01f6e25ce07dca59d51518a22634e710716
Thomas LINTANF <thomas.lintanf@efrei.net>
2020-04-23 10:01:43 +0200

    Created simple interractive Button (task 1)

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,19 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <h1>Clicked {{ count }} times.</h1>
+    <button @click="iAmClicked()">Click me!</button>
   </div>
 </template>
-
+ 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return { count: 0 };
+  },
+  methods: {
+    iAmClicked() {
+      this.count++;
+    }
   }
-}
+};
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
