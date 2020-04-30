af01922912c4505187c3bdc8dc730236f21782d8
Thomas LINTANF <thomas.lintanf@efrei.net>
2020-04-30 09:48:26 +0200

    Create NewParticipant Form

---
+++ src/components/NewParticipantForm.vue
@@ -0,0 +1,20 @@
+<template>
+  <form @submit.prevent="add()">
+    <label>Firstname</label>
+    <input v-model="firstname" type="text" />
+    <label>Lastname</label>
+    <input v-model="lastname" type="text" />
+    <button>Add new participant</button>
+  </form>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      newParticipant : {}
+      };  
+    },
+  }
+};
+</script>
